cedbaf152db1d30f04f5d4af14f65ee565760caa
raielin@gmail.com <raielin@gmail.com>
2014-11-26 20:13:15 -0500

    Compiled handlebars templates.

---
+++ app/templates/templates.js
@@ -0,0 +1,6 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['home'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<h1>Welcome to Bob's Bagels!</h1>\n<p>best bagels. best prices. freshest ingredients. all made with love.</p>\n";
+  },"useData":true});
+})();
